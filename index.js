bb5540dfc03ca892e9824845da0a83e377be4507
Richmond Nkansah <richmondnkansah@gmail.com>
2022-09-22 17:33:18 -0400

    commit tree 667dea72b3b64faa90d1cdc5613c371d25145000

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+  }
+
+  
+  function writeCards(names, events){
+    const newArrey=[];
+    for(let i = 0; i < names.length; i++){
+        newArrey.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+    }
+    return newArrey;
+  }
+
+
+  function countDown(positiveInt){
+    let i = 0;
+    while(positiveInt >= i){
+        console.log(positiveInt);
+    positiveInt--;
+    }
+   
+
+  }
